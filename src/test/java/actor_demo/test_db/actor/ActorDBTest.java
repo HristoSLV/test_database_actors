2d8127680f246df3cde9ee87a802201a6ac26100
svywx <svywxz@gmail.com>
2024-06-29 02:40:52 +0300

    initial profiles commit

--- src/test/java/actor_demo/test_db/actor/ActorDBTest.java
+++ src/test/java/actor_demo/test_db/actor/ActorDBTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@ActiveProfiles()
+@ActiveProfiles("wat")
 public class ActorDBTest {
     @Autowired
     private ActorRepository actorRepository;

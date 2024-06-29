0a435816a5ad6e973facbfbe091b469f13942255
svywx <svywxz@gmail.com>
2024-06-29 19:21:00 +0300

    slight update to configs

--- src/test/java/actor_demo/test_db/actor/ActorDBTest.java
+++ src/test/java/actor_demo/test_db/actor/ActorDBTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -21,5 +23,11 @@ public class ActorDBTest {
 
         assertNotNull(foundEntity);
         assertEquals(actorEntity.getName(), foundEntity.getName());
+    }
+
+    @Test
+    public void testHowManyRows(){
+        List<ActorEntity> actorEntityList = actorRepository.findAll();
+        assertEquals(7, actorEntityList.size());
     }
 }

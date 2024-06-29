9cf9779f3bea35ca2203d05dd1a1181424d81468
svywx <svywxz@gmail.com>
2024-06-29 03:51:00 +0300

    Finished profiles commit

--- src/test/java/actor_demo/test_db/actor/ActorDBTest.java
+++ src/test/java/actor_demo/test_db/actor/ActorDBTest.java
@@ -5,23 +5,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@ActiveProfiles("wat")
+@ActiveProfiles({"testh2", "testsql"})
 public class ActorDBTest {
     @Autowired
     private ActorRepository actorRepository;
 
-
     @Test
-    public void givenGenericEntityRepository_whenSaveAndRetreiveEntity_thenOK() {
+    public void givenGenericEntityRepository_whenSaveAndRetrieveEntity_thenOK() {
         ActorEntity actorEntity = actorRepository
                 .save(new ActorEntity("testActor", 28));
         ActorEntity foundEntity = actorRepository.findById(actorEntity.getId()).orElse(null);
 
         assertNotNull(foundEntity);
         assertEquals(actorEntity.getName(), foundEntity.getName());
+    }
+
+    @Test
+    public void CheckDatabaseSize(){
+        List<ActorEntity> actorEntityList = actorRepository.findAll();
+        assertEquals(7, actorEntityList.size());
     }
 }

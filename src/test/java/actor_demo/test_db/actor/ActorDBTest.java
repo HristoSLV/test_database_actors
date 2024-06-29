f7354d5013f3d182ef836366eae252ce68e61269
svywx <svywxz@gmail.com>
2024-06-29 18:10:59 +0300

    Docker. Docker does not run, unknown cause. v3

--- src/test/java/actor_demo/test_db/actor/ActorDBTest.java
+++ src/test/java/actor_demo/test_db/actor/ActorDBTest.java
@@ -3,7 +3,13 @@ package actor_demo.test_db.actor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
@@ -11,10 +17,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@ActiveProfiles({"testh2", "testsql"})
+@ActiveProfiles({"testsql"})
+@Testcontainers
 public class ActorDBTest {
     @Autowired
     private ActorRepository actorRepository;
+
+    @Container
+    //@ServiceConnection
+    static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.1.0").withDatabaseName("db_test_testcontainer");
+
+    @DynamicPropertySource
+    static void dynamicConfiguration(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", mySQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", mySQLContainer::getUsername);
+        registry.add("spring.datasource.password", mySQLContainer::getPassword);
+    }
 
     @Test
     public void givenGenericEntityRepository_whenSaveAndRetrieveEntity_thenOK() {
